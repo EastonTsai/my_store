a9e704f6b315f3134ee7a2fb8b4ca1cb1eda955f
EastonTsai <eastontsai.te@gmail.com>
2023-06-13 16:13:27 +0800

    app.tsx 新增 basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./styles/app.scss"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { useState } from "react"
 import AdminPage from "./pages/AdminPage"
 import LoginPage from "pages/LoginPage"
 import ProductPage from "./pages/ProductPage"
@@ -9,19 +8,16 @@ import HomePage from "./pages/HomePage"
 
 const basename = process.env.PUBLIC_URL
 const App = () => {
-  const [colorModal, setColorModal] = useState<string>('')
   return (
-    <div className={colorModal}>
-      <BrowserRouter basename={basename}>
-        <Routes>
-          <Route path='/admin' element={<AdminPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/product' element={<ProductPage />} />
-          <Route path='/cart' element={<CartPage />} />
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route path='/admin' element={<AdminPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/product' element={<ProductPage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App
